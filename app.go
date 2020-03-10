c9025678eb05010041ea88b181f7546d3e85f0a3
uraankhayayaal <uraankhayayaal@gmail.com>
2020-03-10 22:11:34 +0300

    updates

--- app.go
+++ app.go
@@ -15,17 +15,15 @@ type Urls []struct {
 	Port string `json:"port"`
 }
 
-var urls Urls
-
 func main() {
-	readJson()
+	urls := readJson()
 
-	f()
+	f(urls)
 
 	fmt.Scanln()
 }
 
-func f() {
+func f(urls Urls) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in f", r)
@@ -33,27 +31,27 @@ func f() {
 	}()
 
 	for i := 0; i < len(urls); i++ {
-		doReq(i)
+		go doReq(i, urls[i].IP, urls[i].Port)
 	}
 }
 
-func doReq(i int) {
+func doReq(i int, ip string, port string) {
 	t_start := time.Now()
 
-	err := os.Setenv("HTTP_PROXY", "http://"+urls[i].IP+":"+urls[i].Port)
+	err := os.Setenv("HTTP_PROXY", "http://"+ip+":"+port)
 	if err != nil {
 		fmt.Println(i, err)
 		return
 	}
 
-	proxyUrl, err := url.Parse("http://" + urls[i].IP + ":" + urls[i].Port)
+	proxyUrl, err := url.Parse("http://" + ip + ":" + port)
 	if err != nil {
 		fmt.Println(i, err)
 		return
 	}
 
-	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}, Timeout: 5 * time.Second}
-	res, err := myClient.Get("http://2children.ru/news/index")
+	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}, Timeout: 10 * time.Second}
+	res, err := myClient.Get("http://2children.ru/news/index?NewsSearch%5Bword%5D=%D0%B4%D0%B5%D1%82%D0%B8")
 	if err != nil {
 		fmt.Println(i, err)
 		return
@@ -61,19 +59,17 @@ func doReq(i int) {
 
 	t_end := time.Now()
 	fmt.Println(i, res.StatusCode, t_end.Sub(t_start))
-
-	doReq(i + 1)
 }
 
-func readJson() {
+func readJson() Urls {
+	var urls Urls
 	jsonFile, err := os.Open("proxies.json")
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	defer jsonFile.Close()
-
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-
 	json.Unmarshal(byteValue, &urls)
+
+	return urls
 }
